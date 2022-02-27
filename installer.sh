01d023fc69f23a5846d64fbb1cc6e61bc2c41eb3
emil237 <95539537+emil237@users.noreply.github.com>
2022-02-27 18:21:01 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -4,11 +4,11 @@
 #
 echo " download and install plugin xstreamity "
 ################################################################################
-version=3.58
+version=3.59
 OPKGINSTALL=opkg install --force-overwrite
 MY_URL="https://raw.githubusercontent.com/emil237/xtreamity/main"
-MY_IPK="xstreamity_3.58_all.ipk"
-MY_DEB="xstreamity_3.58_all.deb"
+MY_IPK="xstreamity_3.59_all.ipk"
+MY_DEB="xstreamity_3.59_all.deb"
 ##############################################################################
 # remove old plugin #
 opkg remove enigma2-plugin-extensions-xstreamity
@@ -50,6 +50,8 @@ echo "**************************************************************************
 wait
 killall -9 enigma2
 exit 0
+
+
 
 
 
