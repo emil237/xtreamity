371b8a55fb5cff57847f32ee5fd861277d30cd82
emil237 <marianatalaat1@gmail.com>
2022-08-06 09:56:56 -0700

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,136 +1,163 @@
 #!/bin/sh
-
-##setup command=wget https://raw.githubusercontent.com/emil237/xtreamity/main/installer.sh -O - | /bin/sh
+# ###########################################
+# SCRIPT : DOWNLOAD AND INSTALL XSTREAMITY
+# ###########################################
 #
-echo " download and install plugin xstreamity "
-################################################################################
-version=3.69
-OPKGINSTALL=opkg install --force-overwrite
-MY_URL="https://raw.githubusercontent.com/emil237/xtreamity/main"
-MY_IPK="xstreamity_3.69_all.ipk"
-MY_DEB="xstreamity_3.69_all.deb"
-##############################################################################
-# remove old plugin #
-opkg remove enigma2-plugin-extensions-xstreamity
+# Command: wget https://raw.githubusercontent.com/emil237/xtreamity/main/installer.sh -qO - | /bin/sh
+#
+# ###########################################
 
-rm -rf /usr/lib/enigma2/python/Plugins/Extensions/XStreamity/
+###########################################
+# Configure where we can find things here #
+TMPDIR='/tmp'
+VERSION='3.70'
+PACKAGE='enigma2-plugin-extensions-xstreamity'
+MY_URL='https://raw.githubusercontent.com/emil237/xtreamity/main'
 
-echo "Ok Removed Old Plugin"
-
-#################################################################################
-
-# Download and install plugin #
-echo " DOWNLOAD AND INSTALL PLUGIN "
-cd /tmp 
-rm -rf * > /dev/null 2>&1
-set -e
-     wget "$MY_URL/$MY_IPK"
-  wait
-     wget "$MY_URL/$MY_DEB"
-
- if which dpkg > /dev/null 2>&1; then
-		dpkg -i --force-overwrite $MY_DEB; apt-get install -f -y
-	else
-		opkg install --force-overwrite $MY_IPK
-	fi
-echo "==========================================================================================================================="
-set +e
-cd ..
-wait
-rm -f /tmp/$MY_IPK
-rm -f /tmp/$MY_DEB
-	if [ $? -eq 0 ]; then
-echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
+####################
+#  Image Checking  #
+if which opkg > /dev/null 2>&1; then
+    STATUS='/var/lib/opkg/status'
+    OSTYPE='Opensource'
+    PKGEXP3='exteplayer3'
+    PKGGPLY='gstplayer'
+    OPKG='opkg update'
+    OPKGINSTAL='opkg install'
+    OPKGREMOV='opkg remove --force-depends'
+else
+    STATUS='/var/lib/dpkg/status'
+    OSTYPE='DreamOS'
+    PKGBAPP='gstreamer1.0-plugins-base-apps'
+    OPKG='apt-get update'
+    OPKGINSTAL='apt-get install'
+    OPKGREMOV='apt-get purge --auto-remove'
+    DPKINSTALL='dpkg -i --force-overwrite'
 fi
-		echo "********************************************************************************"
+
+##################################
+# Remove previous files (if any) #
+rm -rf $TMPDIR/"${PACKAGE:?}"* > /dev/null 2>&1
+
+######################
+#  Remove Old Plugin #
+if grep -qs "Package: $PACKAGE" $STATUS ; then
+    echo "   >>>>   Remove old version   <<<<"
+    if [ $OSTYPE = "Opensource" ]; then
+        $OPKGREMOV $PACKAGE
+        echo ""
+        sleep 2; clear
+    else
+        $OPKGREMOV $PACKAGE
+        echo ""
+        sleep 2; clear
+    fi
+else
+    echo "   >>>>   No Older Version Was Found   <<<<"
+    sleep 1
+    echo ""; clear
+fi
+
+#####################
+# Package Checking  #
+if [ $OSTYPE = "Opensource" ]; then
+    if grep -qs "Package: $PKGEXP3" $STATUS ; then
+        echo "$PKGEXP3 found in device..."
+        sleep 1; clear
+    else
+        echo "Need to install $PKGEXP3"
+        echo
+        echo "Opkg Update ..."
+        $OPKG > /dev/null 2>&1
+        echo " Downloading $PKGEXP3 ......"
+        echo
+        $OPKGINSTAL $PKGEXP3
+        sleep 1; clear
+    fi
+    if grep -qs "Package: $PKGGPLY" $STATUS ; then
+        echo "$PKGGPLY found in device..."
+        sleep 1; clear
+    else
+        echo "Need to install $PKGGPLY"
+        echo
+        echo "Opkg Update ..."
+        $OPKG > /dev/null 2>&1
+        echo " Downloading $PKGGPLY ......"
+        echo
+        $OPKGINSTAL $PKGGPLY
+        sleep 1; clear
+    fi
+
+elif [ $OSTYPE = "DreamOS" ]; then
+    if grep -qs "Package: $PKGBAPP" $STATUS ; then
+        echo " $PKGBAPP found in device..."
+        sleep 1; clear
+    else
+        echo "Need to install  $PKGBAPP"
+        echo
+        echo "APT Update ..."
+        $OPKG > /dev/null 2>&1
+        echo " Downloading  $PKGBAPP ......"
+        echo
+        $OPKGINSTAL  $PKGBAPP -y
+        sleep 1; clear
+    fi
+fi
+
+if [ $OSTYPE = "Opensource" ]; then
+    if grep -qs "Package: $PKGEXP3" $STATUS ; then
+        echo
+    else
+        echo "Feed Missing $PKGEXP3"
+        echo "Sorry, the plugin will not be install"
+        exit 1
+    fi
+    if grep -qs "Package: $PKGGPLY" $STATUS ; then
+        echo
+    else
+        echo "Feed Missing $PKGGPLY"
+        echo "Sorry, the plugin will not be install"
+        exit 1
+    fi
+elif [ $OSTYPE = "DreamOS" ]; then
+    if grep -qs "Package: $PKGBAPP" $STATUS ; then
+        echo
+    else
+        echo "Feed Missing $PKGBAPP"
+        echo "Sorry, the plugin will not be install"
+        exit 1
+    fi
+fi
+###################
+#  Install Plugin #
+if [ $OSTYPE = "Opensource" ]; then
+    echo "Insallling xstreamity plugin Please Wait ......"
+    wget $MY_URL/${PACKAGE}_${VERSION}_all.ipk -qP $TMPDIR
+    $OPKGINSTAL $TMPDIR/${PACKAGE}_${VERSION}_all.ipk
+else
+    echo "Insallling xstreamity plugin Please Wait ......"
+    wget $MY_URL/${PACKAGE}_${VERSION}.deb -qP $TMPDIR
+    $DPKINSTALL $TMPDIR/${PACKAGE}_${VERSION}.deb; $OPKGINSTAL -f -y 
+fi
+
+##################################
+# Remove previous files (if any) #
+rm -rf $TMPDIR/"${PACKAGE:?}"* > /dev/null 2>&1
+
+sleep 1; clear
+echo ""
+echo "***********************************************************************"
+echo "**                                                                    *"
+echo "**       xstreamity : $VERSION                             *"
 echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
 sleep 4;
 		echo ". >>>>         RESTARING     <<<<"
 echo "**********************************************************************************"
-wait
-killall -9 enigma2
+echo ""
+
+if [ $OSTYPE = "Opensource" ]; then
+    killall -9 enigma2
+else
+    systemctl restart enigma2
+fi
+
 exit 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
