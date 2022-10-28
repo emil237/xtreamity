7815e8d104d286ee606deadc81e2375f3f78e197
emil237 <marianatalaat1@gmail.com>
2022-10-28 20:13:28 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -10,7 +10,7 @@
 ###########################################
 # Configure where we can find things here #
 TMPDIR='/tmp'
-VERSION='3.72'
+VERSION='3.76'
 PACKAGE='enigma2-plugin-extensions-xstreamity'
 MY_URL='https://raw.githubusercontent.com/emil237/xtreamity/main'
 
